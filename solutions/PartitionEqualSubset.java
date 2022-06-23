08cb25327a21da58f23569d22bf383503d322ffc
Ashish <ashish21.01agarwal@gmail.com>
2022-06-23 18:58:01 +0530

    Added PartitionEqualSubset.java

--- solutions/PartitionEqualSubset.java
+++ solutions/PartitionEqualSubset.java
@@ -1,35 +1,39 @@
-416. Partition Equal Subset Sum
-Medium
+/**
+ *
+ * 416. Partition Equal Subset Sum
+ * Medium
+ * 
+ * 7811
+ * 
+ * 123
+ * 
+ * Add to List
+ * 
+ * Share
+ * Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,5,11,5]
+ * Output: true
+ * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ * Example 2:
+ * 
+ * Input: nums = [1,2,3,5]
+ * Output: false
+ * Explanation: The array cannot be partitioned into equal sum subsets.
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ * 
+*/
 
-7811
 
-123
-
-Add to List
-
-Share
-Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
-
- 
-
-Example 1:
-
-Input: nums = [1,5,11,5]
-Output: true
-Explanation: The array can be partitioned as [1, 5, 5] and [11].
-Example 2:
-
-Input: nums = [1,2,3,5]
-Output: false
-Explanation: The array cannot be partitioned into equal sum subsets.
- 
-
-Constraints:
-
-1 <= nums.length <= 200
-1 <= nums[i] <= 100
-
-*****************************************************************
 
 import java.util.*;
 
